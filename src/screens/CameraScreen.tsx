2ada5b44f8619d493d45402b3830113d6ba24888
Gloireski <belemgloirebekoutou8@gmail.com>
2025-09-04 09:48:54 +0200

    add nav and screen

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,13 +1,92 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 
-export default function CameraScreen() {
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text  style={{ color: '#lightgray', fontWeight: "bold", fontSize: 24 }}
-                >
-                    Camera Screen
-            </Text>
-        </View>
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
     );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} />
+      <View style={{ 
+        // position: 'absolute',
+        bottom: 64,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        width: '100%',
+        paddingHorizontal: 64,
+      }}>
+        <TouchableOpacity style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+          <MaterialCommunityIcons name="camera" size={60} color="white" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            {
+              Platform.OS === 'ios' ?
+                <MaterialIcons name="flip-camera-ios" size={60} color="white" />
+                :
+                <MaterialIcons name="flip-camera-android" size={60} color="white" />
+            }
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
